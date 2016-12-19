505ecfafe8a4519e4af7c35193efe38fb27b287b
Daniel Longest <dlongest@gmail.com>
2016-12-18 19:33:30 -0500

    Problem 18 both parts

---
+++ Advent.Core/Problem18/Traps.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent.Core.Problem18
+{
+    public class Traps
+    {
+        private string[] traps = new string[] { "^^.", ".^^", "^..", "..^" };
+
+        public string Next(string row)
+        {
+            var withShadowTiles = string.Format(".{0}.", row);
+
+            return string.Join("", Enumerable.Range(0, row.Length)
+                                             .Select(i =>
+                                                        new
+                                                        {
+                                                            Index = i,
+                                                            PriorTiles = withShadowTiles.Substring(i, 3)
+                                                        })
+                                             .Select(a => ItsATrap(a.PriorTiles))
+                                             .Select(b => b ? "^" : "."));
+        }
+
+        public int SafeSpots(string row)
+        {
+            return row.Count(a => a == '.');
+        }
+
+        public int SafeSpots(IEnumerable<string> rows)
+        {
+            return rows.Select(r => SafeSpots(r)).Sum();
+        }
+
+
+        private bool ItsATrap(string tiles)
+        {
+            return this.traps.Contains(tiles);
+        }
+    }
+
+    public static class TrapsExtensions
+    {
+        public static IEnumerable<string> Next(this Traps traps, string firstRow, int rowsToCreate, bool includefirstRow = false)
+        {        
+            var row = firstRow;
+
+            if (includefirstRow)
+                yield return firstRow;
+
+            foreach (var index in Enumerable.Range(0, rowsToCreate))
+            {
+                row = traps.Next(row);
+                yield return row;
+            }
+        }        
+    }
+}
---
+++ Advent.Tests/Problem18/TrapsTests.cs
@@ -0,0 +1,93 @@
+﻿using Advent.Core.Problem18;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Advent.Tests.Problem18
+{
+    public class TrapsTests
+    {
+        [Fact]
+        public void Next_Creates_Correct_NextRow_FromInput()
+        {
+            var sut = new Traps();
+
+            var next = sut.Next("..^^.");
+
+            Assert.Equal(".^^^^", next);
+        }
+
+        [Fact]
+        public void Next_CreatesCorrectRows_FromInitialInput()
+        {
+            var sut = new Traps();
+
+            var rows = sut.Next("..^^.", 2);
+
+            Assert.Equal(".^^^^", rows.First());
+            Assert.Equal("^^..^", rows.Last());
+        }
+
+        [Fact]
+        public void SafeSpots_CorrectlyCountsSafeSpotsInRow()
+        {
+            var sut = new Traps();
+
+            var actual = sut.SafeSpots("..^^.");
+
+            Assert.Equal(3, actual);
+        }
+
+        [Fact]
+        public void SafeSpots_CorrectlyCountsSafeSpotsInAllRows()
+        {
+            var sut = new Traps();
+
+            var rows = sut.Next("..^^.", 2, includefirstRow: true);
+
+            var actual = sut.SafeSpots(rows);
+
+            Assert.Equal(6, actual);
+        }
+
+        [Fact]
+        public void Next_LongerExample_IsCorrect()
+        {
+            var expected = new string[] { ".^^.^.^^^^", "^^^...^..^", "^.^^.^.^^.", "..^^...^^^",
+                ".^^^^.^^.^", "^^..^.^^..", "^^^^..^^^.", "^..^^^^.^^", ".^^^..^.^^", "^^.^^^..^^"  };
+
+            var sut = new Traps();
+
+            var actual = sut.Next(".^^.^.^^^^", 9, includefirstRow: true);
+
+            Assert.Equal(expected, actual);          
+        }
+
+        [Fact]
+        public void SafeSpots_LongerExample_IsCorrect()
+        {
+            var sut = new Traps();
+
+            var rows = sut.Next(".^^.^.^^^^", 9, includefirstRow: true);
+
+            var actual = sut.SafeSpots(rows);
+
+            Assert.Equal(38, actual);
+        }
+
+        [Fact]
+        public void ActualProblem()
+        {
+            var input = "^.^^^.^..^....^^....^^^^.^^.^...^^.^.^^.^^.^^..^.^...^.^..^.^^.^..^.....^^^.^.^^^..^^...^^^...^...^.";
+            var sut = new Traps();
+
+            var rows = sut.Next(input, 399999, includefirstRow: true);
+
+            var safeSpots = sut.SafeSpots(rows);
+
+        }
+    }
+}
